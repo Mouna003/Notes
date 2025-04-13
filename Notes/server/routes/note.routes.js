030323d3a8d5c86e57ea2b7728f5aa6f6dd9e4a5
Mouna Majdi <145588548+Mouna003@users.noreply.github.com>
2025-04-13 21:36:57 +0100

    Add files via upload

---
+++ Notes/server/routes/note.routes.js
@@ -0,0 +1,18 @@
+import { Router } from 'express';
+import { 
+  getAllNotes, 
+  getOneNote, 
+  createNote, 
+  updateNote, 
+  deleteNote 
+} from '../controllers/note.controller.js';
+
+const router = Router();
+
+router.get('/', getAllNotes);
+router.get('/:id', getOneNote);
+router.post('/', createNote);
+router.put('/:id', updateNote);
+router.delete('/:id', deleteNote);
+
+export default router;  // Make sure this is exported as default
